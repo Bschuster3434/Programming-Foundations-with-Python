d5bbc731ec8ccc98301ab329d0c0898e92b826f4
Brian Schuster <brian.daniel.schuster@gmail.com>
2015-04-09 06:43:11 -0400

    Test Data Created

--- Lesson 3a/entertainment_center.py
+++ Lesson 3a/entertainment_center.py
@@ -4,14 +4,13 @@ import fresh_tomatoes
 #Creating Movie Objects
 ###movie_title, movie_storyline, poster_image, trailer_youtube
 pirates_of_silicon_valley = media.Movie(movie_title = "Pirates of Silicon Valley",
-				   movie_storyline = "The story of the creation of the Personal Computer Revolution",
 				   poster_image = "http://upload.wikimedia.org/wikipedia/en/3/30/Movieposterposv.jpg",
 				   trailer_youtube = "https://www.youtube.com/watch?v=lEyrivrjAuU",)
 				   
 
-queen_of_versailles = media.Movie("The Queen of Versailles", "Billionaire family falls into normalacy", "http://foreveryoungadult.com/_uploads/images/29540/thequeenofversailles__span.jpg", "https://www.youtube.com/watch?v=AdJYzgJ4CwI")
+queen_of_versailles = media.Movie("The Queen of Versailles", "http://foreveryoungadult.com/_uploads/images/29540/thequeenofversailles__span.jpg", "https://www.youtube.com/watch?v=AdJYzgJ4CwI")
 
-jiro_dreams_of_sushi = media.Movie("Jiro Dreams of Sushi", "Documentary following Jiro Ono, sushi master and owner of Sukiyabashi Jiro", "http://upload.wikimedia.org/wikipedia/en/4/47/Jiro_sushi_poster.jpg", "https://www.youtube.com/watch?v=I1UDS2kgqY8")
+jiro_dreams_of_sushi = media.Movie("Jiro Dreams of Sushi", "http://upload.wikimedia.org/wikipedia/en/4/47/Jiro_sushi_poster.jpg", "https://www.youtube.com/watch?v=I1UDS2kgqY8")
 
 movies = [pirates_of_silicon_valley, queen_of_versailles, jiro_dreams_of_sushi]
 
